43fd38986ce62868c7a2a203ff25ea0206d23872
jsong <jsong@appen.com>
2022-03-16 18:02:37 +0800

    upload exam apps

--- mysite/exam/models.py
+++ mysite/exam/models.py
@@ -16,7 +16,7 @@ class User(models.Model):
         verbose_name_plural = '用户信息表'
 
     def __str__(self):
-        return self.email
+        return self.email, self.username
 
 
 GENDER_LIST = ["Male", "Female"]
@@ -24,6 +24,7 @@ AGE_LIST = ["0-18", "18-25", "26-30", "31-35", "36-40", "41-45", "46-50", "51-55
 
 
 class UserInfo(models.Model):
+    user_id = models.IntegerField('用户id')
     gender = models.CharField('性别', choices=[(x, x) for x in GENDER_LIST], max_length=10)
     age = models.IntegerField('年龄', choices=[(x, x) for x in GENDER_LIST])
     work_time = models.CharField('可工作时间', max_length=20)
@@ -35,12 +36,12 @@ class UserInfo(models.Model):
         verbose_name_plural = '详细信息表'
 
     def __str__(self):
-        return self.work_time
+        return self.user_id
 
 
 # 题目表 有：题目描述、题目id、题目答案、附带信息
 class Question(models.Model):
-    question = models.CharField('题目描述', max_length=150)
+    question = models.CharField('题目描述', max_length=100)
     answer = models.CharField('答案', max_length=50)
     score = models.IntegerField('分值')
     type = models.CharField('题目类型', choices=(('单选', '单选'), ('多选', '多选'), ('判断', '判断'), ('文本资源评测', '文本资源评测'),
@@ -50,7 +51,7 @@ class Question(models.Model):
         abstract = True  # 抽象类，只用作继承用，不会生成表
 
     def __str__(self):
-        return self.question
+        return self.question, self.answer, self.score, self.type
 
 
 # 根据题目类型 一个题目类型就是一个class
@@ -87,32 +88,59 @@ class QuestionJudge(Question):
         verbose_name = '判断'
         verbose_name_plural = '判断题型'
 
+    def __str__(self):
+        return self.choice
 
-# 文本资源评测
+
+# 文本资源评测（变形多选，按比例得分）
 class QuestionText(Question):
-    choice = models.CharField('T|F', choices=(('True', 'True'), ('False', 'False')), max_length=40)
+    a = models.CharField('A选项', max_length=20)
+    b = models.CharField('B选项', max_length=20)
+    c = models.CharField('C选项', max_length=20)
+    d = models.CharField('D选项', max_length=20)
+    e = models.CharField('E选项', max_length=20)
+    f = models.CharField('F选项', max_length=20)
+    g = models.CharField('G选项', max_length=20)
+    h = models.CharField('H选项', max_length=20)
 
     class Meta:
         verbose_name = '文本'
         verbose_name_plural = '文本资源评测'
 
 
-# 判断题型
-class QuestionJudge(Question):
-    choice = models.CharField('T|F', choices=(('True', 'True'), ('False', 'False')), max_length=40)
+# 医学资源评测（变形单选，下拉框实现）
+class QuestionMedicine(Question):
+    a = models.CharField('A选项', max_length=20)
+    b = models.CharField('B选项', max_length=20)
+    c = models.CharField('C选项', max_length=20)
 
     class Meta:
-        verbose_name = '判断'
-        verbose_name_plural = '判断题型'
+        verbose_name = '医学'
+        verbose_name_plural = '医学资源评测'
 
 
-# 判断题型
-class QuestionJudge(Question):
-    choice = models.CharField('T|F', choices=(('True', 'True'), ('False', 'False')), max_length=40)
+# 论述题型
+class QuestionDiscuss(Question):
+    brief = models.CharField('简介', max_length=150)
 
     class Meta:
-        verbose_name = '判断'
-        verbose_name_plural = '判断题型'
+        verbose_name = '论述'
+        verbose_name_plural = '论述题型'
+
+    def __str__(self):
+        return self.brief
+
+
+# 文本听写  根据音频转写文本
+class QuestionAudio(Question):
+    audio_url = models.CharField('音频链接', max_length=150)
+
+    class Meta:
+        verbose_name = '音频'
+        verbose_name_plural = '音频转写题型'
+
+    def __str__(self):
+        return self.audio_url
 
 
 # 试卷表 有：描述、简介、重考次数、附带信息
@@ -122,6 +150,10 @@ class TestPaper(models.Model):
     re_exam = models.IntegerField(default=0)
     extra_info = models.CharField(max_length=100, null=True, default=None)
 
+    # 是否活跃
+    is_active = models.BooleanField(null=False, default=False)
+    pass_score = models.IntegerField('通过分数', default=0)
+
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间", null=True, blank=True)
 
@@ -130,13 +162,14 @@ class TestPaper(models.Model):
         verbose_name_plural = '试卷表'
 
     def __str__(self):
-        return self.description
+        return self.description, self.introduction
 
 
 # 题库表 有：试卷id、题目id
 class QestionBank(models.Model):
     test_id = models.IntegerField('试卷id')
     question_id = models.IntegerField('题目id')
+    order = models.IntegerField()
     is_abandon = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
@@ -147,4 +180,49 @@ class QestionBank(models.Model):
         verbose_name_plural = '题库表，存储试卷所有的题目'
 
     def __str__(self):
-        return self.test_id
+        return self.test_id, self.question_id, self.order
+
+
+# 考试记录表
+class Record(models.Model):
+    # 记录用户的每一次考试的每一道题
+    user_id = models.IntegerField('用户id')
+    test_id = models.IntegerField('试卷id')
+    question_id = models.IntegerField('题目id')
+
+    user_answer = models.TextField('用户答案')
+    user_score = models.IntegerField('用户得分')
+
+    exam_batch = models.IntegerField('考试批次')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间", null=True, blank=True)
+
+    class Meta:
+        verbose_name = '考试记录'
+        verbose_name_plural = '记录用户的每一次考试的每一道题的情况'
+
+    def __str__(self):
+        return self.test_id, self.question_id, self.user_id, self.user_score
+
+
+# 考试信息表
+class ExamInfo(models.Model):
+    # 记录用户的每一次考试的每一道题
+    user_id = models.IntegerField('用户id')
+    test_id = models.IntegerField('试卷id')
+    exam_score = models.IntegerField('用户总得分')
+    remark = models.CharField('备注', max_length=100)
+    exam_time = models.IntegerField('考试次数')
+    is_pass = models.BooleanField('是否通过', default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间", null=True, blank=True)
+
+    class Meta:
+        verbose_name = '考试记录'
+        verbose_name_plural = '记录用户的每一次考试的每一道题的情况'
+
+    def __str__(self):
+        return self.test_id,  self.user_id, self.exam_score
+
